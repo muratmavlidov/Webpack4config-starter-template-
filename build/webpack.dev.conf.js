e094321c58f595dbbcd88b608c1460cf56284b2e
Murat Mavlidov <mmavlidov96@mail.ru>
2019-03-16 22:45:55 +0300

    starter template to create landing page on webpack4

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf.js');
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+    mode: 'development',
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: baseWebpackConfig.externals.paths.dist,
+        port: 8081,
+        overlay: {
+            warning: false,
+            errors: true
+        }
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map'
+        })
+    ]
+});
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(devWebpackConfig);
+});
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './scripts/common';
+import './css/main.css';
+import './sass/main.sass';
